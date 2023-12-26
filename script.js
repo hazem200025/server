43fa3b9d509563867c69e2a9640f33b295f6d59c
hazem200025 <153329457+hazem200025@users.noreply.github.com>
2023-12-26 01:33:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const socket = io(); // Connects to the server's domain by default
+const videoGrid = document.getElementById('video-grid');
+
+// Update the PeerJS configuration for online deployment
+const myPeer = new Peer(undefined, {
+  host: '/', // Connect to the server's domain by default
+  port: '443', // Use the default HTTPS port
+  secure: true, // Use secure connections (HTTPS)
+  path: '/myapp' // Ensure this matches the path set while starting the PeerJS server
+});
+
+myPeer.on('error', (err) => {
+  console.error('PeerJS Error:', err);
+});
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000; // Use an environment variable for the port
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
